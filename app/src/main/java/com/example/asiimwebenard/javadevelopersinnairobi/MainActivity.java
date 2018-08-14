8bdcbbca4ea1e304f1235cbdae3033a7756d6102
King-Benx <kingbenx17@gmail.com>
2018-08-14 09:38:07 +0300

    feat(configuring changes): Handling Configuration changes
    This commit ensures that an activity doesn'' get destroyed
    when the screen orientation changes.
    
    [Delivers #156005187]

--- app/src/main/java/com/example/asiimwebenard/javadevelopersinnairobi/MainActivity.java
+++ app/src/main/java/com/example/asiimwebenard/javadevelopersinnairobi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.asiimwebenard.javadevelopersinnairobi;
 
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -7,6 +8,9 @@ import android.support.v7.widget.RecyclerView;
 
 
 public class MainActivity extends AppCompatActivity {
+    public final static String LIST_STATE_KEY= "recycler_list_state";
+    private RecyclerView.LayoutManager layoutManager;
+    Parcelable listState= null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,25 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new GridLayoutManager(this,2));
         recyclerView.setAdapter(recyclerViewAdapter);
     }
+
+    protected void onSaveInstanceState(Bundle state){
+        super.onSaveInstanceState(state);
+        state.putParcelable(LIST_STATE_KEY, listState);
+    }
+
+    protected void onRestoreInstanceState(Bundle state) {
+        super.onRestoreInstanceState(state);
+        if(state != null)
+            listState = state.getParcelable(LIST_STATE_KEY);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (listState != null) {
+            layoutManager.onRestoreInstanceState(listState);
+        }
+    }
+
 
 }
