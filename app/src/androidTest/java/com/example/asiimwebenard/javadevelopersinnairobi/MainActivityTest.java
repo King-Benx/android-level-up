788d1f331a4dd2ac8d415feac14ea9a75a9d7371
King-Benx <kingbenx17@gmail.com>
2018-08-22 10:38:12 +0300

    feat(ui-tests): Implements UI tests
    This commit delivers the front end tests to ensure
    quality assurance.
    
    [Delivers #157354404]

---
+++ app/src/androidTest/java/com/example/asiimwebenard/javadevelopersinnairobi/MainActivityTest.java
@@ -0,0 +1,35 @@
+package com.example.asiimwebenard.javadevelopersinnairobi;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void backgroundImageShouldBeDisplayed(){
+        onView(withId(R.id.background_image)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void recylerViewShouldBeDisplayed(){
+        onView(withId(R.id.recycler_view)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void swipeToRefeshdisplayed(){
+        onView(withId(R.id.swipe_refresh)).check(matches(isDisplayed()));
+    }
+
+}
